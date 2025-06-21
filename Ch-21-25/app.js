dc4f7c3970cf33173d7337c1d9f1d181d82b589d
Huzaifamalic <huzaifamalick506@gmail.com>
2025-06-21 20:07:40 +0500

    add 21 to 25

---
+++ Ch-21-25/app.js
@@ -0,0 +1,86 @@
+// Question :1
+// Answer: 1
+
+// var fname= prompt("Enter your first name:");
+// var lname= prompt("Enter your last name:");
+// var fullname= fname + " " + lname;
+// console.log("Your full name is: " + fullname);
+
+// Question :2
+// Answer: 2
+
+// var user_fav= prompt("Enter your fav phne:");
+
+// for (var i = 0; i < user_fav.length; i++) {
+
+// }
+// console.log(user_fav );
+// console.log("Lenght of " + user_fav.length  )
+
+// Question :3
+// Answer: 3
+
+// var cuntery_name ='pakistan'
+// var indexOf = cuntery_name.indexOf('n');
+// console.log(cuntery_name)
+// console.log(indexOf);
+
+// Question :4
+// Answer: 4
+
+// var str = "Hello World";
+// var lastIndex = str.length -2;
+// console.log(str + " Last index is: " + lastIndex);
+
+// Question :5
+// Answer: 5
+//   var word = "Pakistani";
+//   var find_char = word.charAt(3);
+//     console.log("Character at index 3 is: " + find_char);
+
+// Question :6
+// Answer: 6
+// var str = "karachi";
+// var about= "city of lights";
+// var concat =str.concat(" "+ " " + about);
+// console.log(concat);
+
+// Question :7
+// Answer: 7
+
+// var city = "Hyderabad";
+// var replace = city.replace("Hyder", "Islam");
+// console.log(replace);
+// Question :8
+// Answer: 8
+//  var message = "Ali and Sami are best friends. They play cricket and football together.";
+//   var updatedMessage = message.replaceAll("and", "&");
+//   console.log(updatedMessage);
+
+// Question :9
+// Answer: 9
+
+//  var str = "472";
+//   var num = Number(str); 
+
+//   console.log("Value: " + str + "<br>");
+//   console.log("Type: " + typeof str + "<br><br>");
+
+//   console.log("Value: " + num + "<br>");
+//   console.log("Type: " + typeof num);
+
+// Question :10
+// Answer: 10
+// var user_input = prompt("Enter a number: ");
+// console.log(user_input.toLocaleUpperCase())
+
+// Question :12
+// Answer: 12
+
+// var num = 35.36;
+//   var str = num.toString();           
+//   var result = str.replace(".", "");  
+//   console.log("Result: " + result);
+
+// Question :13
+// Answer: 13
