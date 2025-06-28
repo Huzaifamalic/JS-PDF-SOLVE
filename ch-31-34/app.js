64f1ad925990b935560a93d8ae7e407196c87ebe
Huzaifamalic <huzaifamalick506@gmail.com>
2025-06-28 20:35:17 +0500

    ch 31 34

---
+++ ch-31-34/app.js
@@ -0,0 +1,93 @@
+// Qestion: 1
+// Answer: 1
+
+var d = new Date();
+
+console.log(d)
+// Qestion: 2
+// Answer: 2
+
+// var months = [
+//   "January", "February", "March", "April", "May", "June",
+//   "July", "August", "September", "October", "November", "December"
+// ];
+// var current_month = d.getMonth();
+// console.log("Current Month: " + months[current_month]);'
+
+// Qestion: 3
+// Answer: 3
+
+
+
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var day = d.getDay();
+// console.log("Current Day: " + dayNames[day]);
+
+// Qestion: 4
+// Answer: 4
+// var day = d.getDay();
+// if (day === 0 || day === 6) {
+//   console.log("It's Fun day");
+// } else {
+//   console.log("It's a regular day");
+// }
+
+// Qestion: 5
+// Answer: 5
+
+// var day = d.getDate();
+// if (day < 16) {
+//   console.log("First fifteen days of the month");
+// }
+// else {
+//     console.log("Last days of the month");
+//     }
+
+
+// Qestion: 6
+// Answer: 6
+
+// var milliseconds = d.getTime();
+
+// console.log("Milliseconds since January 1, 1970: " + milliseconds);
+
+// Qestion: 7
+// Answer: 7
+
+// var hours = d.getHours();
+// if (hours < 12) {
+//   console.log("It's AM");
+// } else {
+//   console.log("It's PM");
+// }
+
+// Qestion: 8
+// Answer: 8
+
+// var laterdate = new Date("2020-12-31");
+// console.log("Later date: " + laterdate);
+
+// Qestion: 9
+// Answer: 9    
+
+// var sartramzan = new Date("2026-06-18");
+
+// var currentDate = d
+
+// var diffInMs = sartramzan - d;
+// var daypass = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+// console.log("Days passed since 1st Ramadan, 2026: " + daypass);
+
+// Qestion: 10
+// Answer: 10
+
+
+// var refrancedate = new Date("2020-01-01");
+// var currentDate = new Date("2025-01-01");
+// var diffInMs = currentDate - refrancedate;
+// var sec = Math.floor(diffInMs / 1000);
+// console.log("Seconds since reference date: "+ refrancedate + sec);
+
+// Qestion: 11
+
+// Answer: 11
